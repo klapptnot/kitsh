6f4e4a44a988c62751f8df0f738f6dfae681a2df
Ari Z3 <klapptnot@gmail.com>
2026-05-05 12:02:36 -0300

    mend ➜ fix tailing null byte & flags

--- lib/rsum.sh
+++ lib/rsum.sh
@@ -9,7 +9,7 @@
 #   -c, --chars   Opt<str> default: "a-zA-Z0-9"
 #   -s, --sep     Opt<str> default: ""
 #   -t, --times   Opt<int> default: 0
-#   -b, --breakl  (flag)   default: false
+#   -b, --break   (flag)   default: false
 #
 # This: rsum -l 5 -c '0-9' -s '-' -t 5 -b
 # Prints something like: 28942-11609-47543-66540-15565
@@ -27,52 +27,45 @@ function rsum {
   while [ "${#}" -gt 0 ]; do
     case "${1}" in
       -l | -l* | --length)
-        if [ -n "${1#*-l}" ]; then
-          length=${1#*-l}
-        elif [ -n "${2}" ]; then
+        if [[ "${1}" == '--length' ]]; then
           length=${2}
           shift 1
+        elif [ -n "${1#-l}" ]; then
+          length=${1#-l}
         fi
-        shift 1
         ;;
       -c | --chars)
         if [ -n "${2}" ]; then
           charset=${2}
           shift 1
         fi
-        shift 1
         ;;
       -s | --sep)
         if [ -n "${2}" ]; then
           sep=${2}
           shift 1
         fi
-        shift 1
         ;;
       -t | -t* | --times)
-        if [ -n "${1#*-t}" ]; then
-          times=${1#*-t}
-        elif [ -n "${2}" ]; then
+        if [[ "${1}" == '--times' ]]; then
           times=${2}
           shift 1
+        elif [ -n "${1#-t}" ]; then
+          times=${1#-t}
         fi
-        shift 1
         ;;
       -b | --break)
         break_line=true
-        shift 1
-        ;;
-      *)
-        shift 1
         ;;
     esac
+    shift 1
   done
 
   [[ ${times} =~ ^[0-9]+$ ]] && {
     local esep="${sep@Q}"
     [ "${esep:0:1}" == '$' ] && esep="${esep:2:-1}"
     esep="${esep:1:-1}"
-    tr -dc "${charset}" < /dev/urandom 2> >(sed 's/^tr:/rsum:/' >&2) | head -c "$((length * times))" | sed "s/\s//g;s/\(.\{1,${length}\}\)/\1${esep}/g;s/${esep}$//"
+    tr -dc "${charset}" < /dev/urandom 2> >(sed 's/^tr:/rsum:/' >&2) | head -c "$((length * times))" | sed "s/\(.\{1,${length}\}\)/\1${esep}/g;s/${esep}$//"
     ${break_line} && echo '' 2> /dev/null
     return
   }
