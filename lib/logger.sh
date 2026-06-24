c2722917cbfc96f8ee1c9681c6e24d89da1bf22d
Ari Z3 <klapptnot@gmail.com>
2026-06-25 08:50:37 +0900

    tune ➜ update logger for simplicity

--- lib/logger.sh
+++ lib/logger.sh
@@ -2,6 +2,7 @@
 # Upstream: https://github.com/klapptnot/kitsh
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+declare -gra LOGGER_LEVEL_NAMES=('OFF' 'CRIT' 'ERROR' 'WARN' 'INFO' 'DEBUG')
 declare -gra LOGGER_LOG_COLORS=(
   ''
   '\x1b[30m\x1b[41m'
@@ -10,58 +11,35 @@ declare -gra LOGGER_LOG_COLORS=(
   '\x1b[34m'
   '\x1b[32m'
 )
-declare -gra LOGGER_LEVEL_NAMES=(
-  'OFF'   # 0
-  'CRIT'  # 1
-  'ERROR' # 2
-  'WARN'  # 3
-  'INFO'  # 4
-  'DEBUG' # 5
-)
-declare -grA LOGGER_LEVEL_NUMS=(
-  ['o']=0
-  ['c']=1
-  ['e']=2
-  ['w']=3
-  ['i']=4
-  ['d']=5
-)
 
-# Normalize log level from env once at load time.
-# Accepts: 0-5, o/c/e/w/i/d, or off/crit/error/warn/info/debug
-# Precedence: LOGGER_LEVEL > LOG_LEVEL > default (info)
-function log::setup {
-  local raw="${1:-${LOGGER_LEVEL:-${LOG_LEVEL:-i}}}"
-  raw="${raw,,}"
+if [[ "${BASH_SOURCE[0]}" != *"${1}" ]]; then
+  readonly _LGGRLEVEL="${1:-4}"
+  readonly _LGGRFORMAT="${2:-short}"
+else
+  readonly _LGGRLEVEL=4
+  readonly _LGGRFORMAT="short"
+fi
 
-  if [[ "${raw}" =~ ^[0-5]$ ]]; then
-    LOGGER_EFFECTIVE_LEVEL="${raw}"
-  elif [[ -n "${raw}" && -n "${LOGGER_LEVEL_NUMS[${raw:0:1}]}" ]]; then
-    LOGGER_EFFECTIVE_LEVEL="${LOGGER_LEVEL_NUMS[${raw:0:1}]}"
-  fi
+declare -gi LOGGER_EFFECTIVE_LEVEL="${_LGGRLEVEL:-4}"
 
-  declare -gri LOGGER_EFFECTIVE_LEVEL="${LOGGER_EFFECTIVE_LEVEL:-4}"
-}
-
-# Print (or not) to output info based on the LOGGER_LEVEL env var
-# LOGGER_LEVEL is `i` by default
-# Usage:
-#   log <level> <fmt> [args...]
-function log {
-  [[ -z "${1}" ]] && return
-  [[ -z "${LOGGER_EFFECTIVE_LEVEL+x}" ]] && log::setup
-
-  : "${1:0:1}"
-  local -ri level="${LOGGER_LEVEL_NUMS[${_,,}]:-0}"
-  ((0 == LOGGER_EFFECTIVE_LEVEL || level > LOGGER_EFFECTIVE_LEVEL)) && return
-
-  # shellcheck disable=SC2059
-  printf -v message "${2}" "${@:3}"
-
-  local color="${LOGGER_LOG_COLORS[level]}"
-  if ((${#1} > 1)); then
-    printf '%b[%(%F %T)T] %5s: %s\x1b[0m\n' "${color}" -1 "${LOGGER_LEVEL_NAMES[level]}" "${message}" &> /dev/stderr
+if [[ -t 1 ]]; then
+  if [[ "${_LGGRFORMAT}" == 'long' ]]; then
+    function _log::format {
+      printf "${LOGGER_LOG_COLORS[${1}]}[%(%F %T)T] %-5s: ${2}\x1b[0m\n" -1 "${LOGGER_LEVEL_NAMES[${1}]}" "${@:3}"
+    }
   else
-    printf '%b%s\x1b[0m\n' "${color}" "${message}" &> /dev/stderr
+    function _log::format {
+      printf "${LOGGER_LOG_COLORS[${1}]}%-5s: ${2}\x1b[0m\n" "${LOGGER_LEVEL_NAMES[${1}]}" "${@:3}"
+    }
   fi
-}
+else
+  function _log::format {
+    printf "[%(%F %T)T] %-5s: ${2}\n" -1 "${LOGGER_LEVEL_NAMES[${1}]}" "${@:3}"
+  }
+fi
+
+function log::crit  { ((LOGGER_EFFECTIVE_LEVEL > 0)) && _log::format 1 "${@}"; } >&2
+function log::err   { ((LOGGER_EFFECTIVE_LEVEL > 1)) && _log::format 2 "${@}"; } >&2
+function log::warn  { ((LOGGER_EFFECTIVE_LEVEL > 2)) && _log::format 3 "${@}"; } >&2
+function log::info  { ((LOGGER_EFFECTIVE_LEVEL > 3)) && _log::format 4 "${@}"; }
+function log::debug { ((LOGGER_EFFECTIVE_LEVEL > 4)) && _log::format 5 "${@}"; }
